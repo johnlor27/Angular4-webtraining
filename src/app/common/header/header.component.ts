e4d232ff8f38c57ebe1757e7a670c7b21772f3ca
Alex Arriaga <alexzander.arriaga@base22.com>
2018-06-28 14:42:14 -0500

    Incluir método logout() vacío para permitir compilación y también inyección del Router y el Authentication service usando el constructor del HeaderComponent

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnChanges, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
+import {AuthenticationService} from '../../../../e2e/app/common/services/authentication.service';
 
 // import { AuthenticationService } from '../../auth/project-list/services/authentication.service';
 
@@ -11,15 +12,16 @@ import {ActivatedRoute, Router} from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
-  // constructor(public router: Router, public _authService: AuthenticationService) { }
+  constructor(public router: Router, public _authService: AuthenticationService) {
+  }
 
   ngOnInit() {
 
   }
 
-  /* logout(){
-       this._authService.logout();
-       this.router.navigate(['/login']);
-   }*/
+  logout() {
+    // this._authService.logout();
+    // this.router.navigate(['/login']);
+  }
 
 }
